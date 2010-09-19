8ce66e5cddd014326ac7bc582ab2a8ac94458e85
dream <dream@mail.i2p>
2010-09-19 04:08:45 +0000

    enableAliases wasn't working, so I tried having it repeatedly call. Still doesn't work though because jetty doesn't even instantiate the servlet until you go to the URL. Hm...

--- enableAliases/src/i2p/dream/AliasEnabler.java
+++ enableAliases/src/i2p/dream/AliasEnabler.java
@@ -7,6 +7,10 @@ package i2p.dream;
 
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javax.servlet.ServletException;
 import javax.servlet.ServletConfig;
 import javax.servlet.http.HttpServlet;
@@ -18,13 +22,55 @@ import org.mortbay.util.FileResource;
  *
  * @author dream
  */
+
+class EnableAliases implements Runnable {
+    public void run() {
+        synchronized (FileResource.class) {
+            FileResource.setCheckAliases(false);
+            System.out.println("NO CHECK ALIASES GOD!");
+        }
+    }
+
+    static final EnableAliases instance = new EnableAliases();
+}
+
+class Repeatedly implements Runnable {
+    private final Runnable task;
+    private final long delay;
+    private final ExecutorService exec;
+    private int amount;
+
+    Repeatedly(int amount, long delay, ExecutorService exec, Runnable task) {
+        this.task = task;
+        this.delay = delay;
+        this.exec = exec;
+        this.amount = amount;
+        new Thread(this).run();
+    }
+    public void run() {
+        for(int i = 0;i<amount;++i) {
+            // to make sure task still happens in the executor thread
+            exec.submit(task);
+            try {
+                Thread.sleep(delay);
+            } catch (InterruptedException ex) {
+                Logger.getLogger(Repeatedly.class.getName()).log(Level.SEVERE, null, ex);
+            }
+        }
+    }
+}
+
 public class AliasEnabler extends HttpServlet {
-    public static void enableAliases() {
-        FileResource.setCheckAliases(false);
+
+    static final ExecutorService exec = Executors.newSingleThreadExecutor();
+
+    static void enableAliases() {
+        exec.submit(EnableAliases.instance);
     }
 
     static {
         enableAliases();
+        new Repeatedly(3,10000,exec,EnableAliases.instance);
     }
 
     @Override
