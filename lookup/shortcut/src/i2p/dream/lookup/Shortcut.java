1a49c4af0e1adc796041274c726c23a539f1b53c
dream <dream@mail.i2p>
2011-09-10 02:18:23 +0000

    Fixing Shortcut to parse URLs correctly when adding an address helper. Also a quick example of how to use rsync over i2p.

--- lookup/shortcut/src/i2p/dream/lookup/Shortcut.java
+++ lookup/shortcut/src/i2p/dream/lookup/Shortcut.java
@@ -1,24 +1,28 @@
 package i2p.dream.lookup;
 
-import java.util.logging.Level;
-import java.util.logging.Logger;
+import net.i2p.I2PAppContext;
+import net.i2p.data.Base32;
+import net.i2p.data.Base64;
+import net.i2p.data.DataFormatException;
+import net.i2p.data.Destination;
+import net.i2p.data.Hash;
+import net.i2p.data.Base64;
+
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import java.io.IOException;
-
-import net.i2p.data.Base32;
-import net.i2p.data.DataFormatException;
-import net.i2p.data.Destination;
-
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import java.io.PrintWriter;
-
-import net.i2p.data.Hash;
-
-import net.i2p.I2PAppContext;
-import net.i2p.data.Base64;
+import java.io.IOException;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.net.URL;
+import java.net.URLEncoder;
+import java.net.URLDecoder;
+import java.net.MalformedURLException;
 
 public class Shortcut extends HttpServlet {
     
@@ -31,18 +35,18 @@ public class Shortcut extends HttpServlet {
                          HttpServletResponse response) 
         throws ServletException, IOException {
 
-        String name = request.getParameter("name");
-        boolean quick = request.getParameter("q") != null;
-        if (name == null) {
+        String bleh = request.getParameter("url");
+        URL url = null;
+        String name = null;
+        if(bleh!=null) {
+            url = new URL(URLDecoder.decode(bleh,"utf-8"));
+            name = url.getHost();
+        }
+
+        final boolean quick = request.getParameter("q") != null;
+        if (url == null) {
             askForName(response);
         } else {
-            if (name.startsWith("http://")) {
-                name = name.substring(7);
-            }
-            int i = name.indexOf('/');
-            if (i > 0) {
-                name = name.substring(0, i);
-            }
             boolean isb32 = false;
             Destination address = null;
             int length = name.length();
@@ -61,18 +65,17 @@ public class Shortcut extends HttpServlet {
             final String b64 = request.getParameter("b64");
             if (b64 == null) {
                 // lookup works for both b32 and addressbook names.
-                System.err.println("nameboo " + name);
                 address = I2PAppContext.getGlobalContext().namingService().lookup(name);
-                // we still want to give it a name though.
+                // we still want to give it a name though, even though b32
                 if(isb32) {
-                    name = null;
+                    name = request.getParameter("name");
                 }
-                showShortcut(response, name, address, quick);
+                showShortcut(response, name, url, address, quick);
                 return;
             } else {
                 if (isb32) {
                     // don't be adding any b32 names to our addressbook now.
-                    name = null;
+                    name = request.getParameter("name"); 
                 }
                 address = new Destination();
                 try {
@@ -87,7 +90,7 @@ public class Shortcut extends HttpServlet {
                     });
                     return;
                 }
-                showShortcut(response, name, address, quick);
+                showShortcut(response, name, url, address, quick);
             }
         }
     }
@@ -117,14 +120,52 @@ public class Shortcut extends HttpServlet {
             public void respond(PrintWriter out) {
                     out.println("<html><head><title>Shortcut Finder</title></head><body>");
                     out.println("<p>Enter a name in your addressbook, or a base64 hash to find the shortcut base32 address for the eepsite.</p>");
-                    out.println("<p><form><input name=\"name\" type=\"text\"/><input name=\"submit\" type=\"submit\" value=\"OK\"/></p>");
+                    out.println("<p><form><input name=\"url\" type=\"text\"/><input type=\"submit\" value=\"OK\"/></p>");
                     out.println("</body></html>");
                 }
             });
     }
 
+    URL addAddressHelper(final URL url, final Destination address, boolean useb32) {
+        final String scheme = url.getProtocol();
+        final String host = url.getHost();
+        final int port = url.getPort();
+        final String authority = url.getAuthority();
+        final String userInfo = url.getUserInfo();
+        final String path = url.getPath();
+        String query = url.getQuery();
+        final String fragment = url.getRef();
+        /* the contextual URL constructor parses 
+           "http://a.b/c/d" + "?i2paddresshelper=..." wrong
+           into "http://a.b/c/?i2paddresshelper=..." (discarding d)
+           so we'll just do it instead.
+        */
+
+        final String param;
+        if(useb32)
+            param = "i2paddresshelper="+Base32.encode(address.getHash().getData())
+                +".b32.i2p";
+        else
+            param = "i2paddresshelper="+address.toBase64();
+
+        if(query==null)
+            query = param;
+        else 
+            query = query + "&" + param;
+        try {
+            return (new URI(scheme,userInfo,host,port,path,query,fragment)).toURL();
+            // these exceptions should never happen since we started 
+            // with a valid URL
+        } catch(URISyntaxException ex) {
+            throw new RuntimeException("The URL wasn't a URI?",ex);
+        } catch(MalformedURLException ex) {
+            throw new RuntimeException("The URL wasn't a URL???",ex);
+        }
+    }
+
     void showShortcut(HttpServletResponse response,
                       final String name,
+                      final URL url,
                       final Destination address,
                       boolean quick)
         throws IOException {
@@ -154,8 +195,9 @@ public class Shortcut extends HttpServlet {
                     out.println("<h1>Name needed</h1>");
                     out.println("<p>Please enter a name for " + b32 + ":");
                     out.println("<form><input type=\"text\" name=\"name\" />");
+                    out.println("<form><input type=\"hidden\" name=\"url\" value=\""+url+"\" />");
                     out.println("<input type=\"hidden\" name=\"b64\" value=\"" + address.toBase64() + "\" />");
-                    out.println("<input type=\"submit\" name=\"submit\" value=\"OK\" />");
+                    out.println("<input type=\"submit\" value=\"OK\" />");
                     out.println("</form></body></html>");
                 }
             });
@@ -163,19 +205,29 @@ public class Shortcut extends HttpServlet {
         }
         if(quick) {
             response.setStatus(HttpServletResponse.SC_MOVED_PERMANENTLY);
-            response.setHeader("Location", "http://" + name + "/?i2paddresshelper=" + address.toBase64());
+            response.setHeader("Location", addAddressHelper(url,address,false).toExternalForm());
         } else {
             respond(response, new Responder() {
                     public void respond(PrintWriter out) {
-                        String b32 = Base32.encode(address.getHash().getData());
-                        out.println("<html><head><title>Jump for "+name+"|"+b32+"</title>");
-                        out.println("</head><body>");
-                        out.println("<h1>"+name+" ready.</h1>");
-                        out.println("<ul>");
-                        out.println("<li>To add this to your addressbook, go <a href=\"http://"+name+"/?i2paddresshelper="+address.toBase64()+"\">here.</a></li>");
-                        out.println("<li>For a shortcut to skip this page go <a href=\"/shortcut/?q=1&name="+name+"\">here.</a></li>");
-                        out.println("<li>To just use the base32 of "+name+" go <a href=\"http://"+b32+".b32.i2p\">here.</a></li>");
-                        out.println("</ul></body></html>");
+                        try {
+                            String b32 = Base32.encode(address.getHash().getData());
+                            out.println("<html><head><title>Jump for "+name+"|"+b32+"</title>");
+                            out.println("</head><body>");
+                            out.println("<h1>"+name+" ready.</h1>");
+                            out.println("<ul>");
+                            out.println("<li>To add this to your addressbook, go <a href=\""+addAddressHelper(url,address,false)+"\">here.</a></li>");
+                            out.println("<li>To copy this into IRC, go <a href=\""+addAddressHelper(url,address,true)+"\">here.</a></li>");
+                            try {
+                                out.println("<li>For a shortcut to skip this page go <a href=\"/shortcut/?q=1&url="+URLEncoder.encode(url.toString(),"UTF-8")
+                                            +"\">here.</a></li>");
+                            } catch(java.io.UnsupportedEncodingException ex) {
+                                throw new RuntimeException("Uh...what no utf-8 support?",ex);
+                            }
+                            out.println("<li>To just use the base32 of "+name+" go <a href=\""+new URL(url.getProtocol(),b32+".b32.i2p",url.getFile())+"\">here.</a></li>");
+                            out.println("</ul></body></html>");
+                        } catch(MalformedURLException ex) {
+                            out.println("<p>error! <pre>"+ex+"</pre></p></body></html>");
+                        }
                     }
                 });
         }
