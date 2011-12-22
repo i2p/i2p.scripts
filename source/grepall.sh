546726ff75a763c7c3457d90dc947e5c6e636dd4
kytv <kytv@mail.i2p>
2011-12-22 00:45:50 +0000

    Add -l to the grep parameter to print the file names. Remove the /dev/null. Remove the dependency on bash

--- source/grepall.sh
+++ source/grepall.sh
@@ -1,7 +1,6 @@
-#!/bin/bash
+#!/bin/sh
 #
 # grep the source
 # maybe grep -R would be better
-# add /dev/null to force filename print - seems like there should be a grep option but guess not
 #
-find . -type f \( -name \*.java -o -name \*.jsp -o -name \*.xml \) -exec grep -i "$@" {} /dev/null \;
+find . -type f \( -name \*.java -o -name \*.jsp -o -name \*.xml \) -exec grep -il "$@" {} \;
