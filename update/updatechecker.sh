05213826968376506c9b73752025bec9cb9da758
kytv <kytv@mail.i2p>
2011-12-22 01:52:53 +0000

    The main change is with trying to get the update URLs from the source itself

--- update/updatechecker.sh
+++ update/updatechecker.sh
@@ -1,4 +1,4 @@
-#
+#!/bin/sh
 # Check to see if all update hosts have the latest
 # sud and su2 files by checking the first 56 bytes of each.
 #
@@ -10,7 +10,13 @@
 #
 EEPPROXY='127.0.0.1:4444'
 
-REL=`grep 'public final static String VERSION' core/java/src/net/i2p/CoreVersion.java | cut -d '"' -f2`
+if [ ! -r core/java/src/net/i2p/CoreVersion.java ]; then
+        echo "ERROR: This script must be run from the source directory." >&2
+        exit 1
+else
+        REL=$(awk -F'"' '/public final static String VERSION/{print $2}' core/java/src/net/i2p/CoreVersion.java)
+fi
+
 if [ -z "$REL" ]
 then
 	echo "Cannot find current version"
@@ -18,28 +24,34 @@ then
 fi
 CHARCOUNT=`echo -n $REL | wc -c`
 
+# Let's try to acquire the URLS from the source.
+SOURCES=$(sed -e '/http:\/\/\(.*\)i2pupdate\.su./!d'  -e 's|\\r\\n\"\s+||g' -e 's/"//g' -e 's/;//g' -e 's/\s//g' apps/routerconsole/java/src/net/i2p/router/web/ConfigUpdateHandler.java)
+
 #SOURCES=`cat updatesources.txt`
 #if [ -z "$SOURCES" ]
 #then
-#	echo "No sources to check"
-#	exit 1
+#      echo "No sources to check"
+#      exit 1
 #fi
-# I guess we could also grep these out of the source but that's too hard
-SOURCES=" \
-	http://echelon.i2p/i2p/i2pupdate.sud         \
-	http://echelon.i2p/i2p/i2pupdate.su2         \
-	http://www.i2p2.i2p/_static/i2pupdate.sud    \
-	http://www.i2p2.i2p/_static/i2pupdate.su2    \
-	http://update.postman.i2p/i2pupdate.sud      \
-	http://update.postman.i2p/i2pupdate.su2      \
-	http://stats.i2p/i2p/i2pupdate.sud           \
-	http://stats.i2p/i2p/i2pupdate.su2           \
-"
+
+#SOURCES=" \
+#       http://echelon.i2p/i2p/i2pupdate.sud         \
+#       http://echelon.i2p/i2p/i2pupdate.su2         \
+#       http://www.i2p2.i2p/_static/i2pupdate.sud    \
+#       http://www.i2p2.i2p/_static/i2pupdate.su2    \
+#       http://update.postman.i2p/i2pupdate.sud      \
+#       http://update.postman.i2p/i2pupdate.su2      \
+#       http://stats.i2p/i2p/i2pupdate.sud           \
+#       http://stats.i2p/i2p/i2pupdate.su2           \
+#"
 
 if [ -z "$I2P" ]
 then
-	echo '$I2P must be set'
-	exit 1
+        echo '$I2P must be set'
+        exit 1
+elif [ ! -e "$I2P/lib/i2p.jar" ]; then
+        echo "ERROR: Could not find i2p.jar" >&2
+        exit 1
 fi
 
 TMPDIR=/tmp/updatecheck$$
