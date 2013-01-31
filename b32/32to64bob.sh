f9e35bbe22ecf8906cff5b230e6502bd80110984
kytv <kytv@mail.i2p>
2013-01-31 02:33:59 +0000

    switch to using sed to snip what we don't want

--- b32/32to64bob.sh
+++ b32/32to64bob.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 # From http://forum.i2p/viewtopic.php?t=4367
 
-b64=$(echo "lookup $1 \nquit\n" | nc localhost 2827|grep AAAA |cut -f2 -d' ')
+temp=$(printf "lookup $1 \nquit\n" | nc localhost 2827) 2>/dev/null
+b64=$(echo $temp | sed 's/BOB\s00\.00\.10\sOK\sOK\s\([A-Za-z0-9~-]\+\)\sOK\sBye!/\1/')
 echo $b64
