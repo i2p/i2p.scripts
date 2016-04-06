1b4a9863807d05f20a90e6c369e25bbf47923bb1
ampernand <ampernand@gmail.com>
2016-04-06 20:52:44 +0000

    irchook.lua
    * report all parts of revision

--- irc/mtnbot/irchook.lua
+++ irc/mtnbot/irchook.lua
@@ -45,7 +45,9 @@ end
 
 function note_netsync_revision_received(new_id, revision, certs, session_id)
    local commiter = certs[1].key.given_name
-   local branch = ""
    -- announce in channel
-   ircsay(channel, string.format("commit by %s on %s ( %s )", commiter, branch,  new_id))
+   ircsay(channel, string.format("commit by %s: ( %s )", commiter, new_id))
+   for k, c in pairs(certs) do
+      ircsay(channel, string.format("%s : %s", c.name, c.value))
+   end
 end
