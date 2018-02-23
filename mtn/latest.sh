a84a92328420f5d5a20de36ce8f6d731978c55c0
zzz <zzz@mail.i2p>
2018-02-23 16:55:52 +0000

    latest.sh sort by date

--- mtn/latest.sh
+++ mtn/latest.sh
@@ -1,17 +1,23 @@
+#!/bin/sh
 #
-# list head of all branches
+# List heads of all branches in the database, sorted by last-changed, latest last.
 #
 # zzz 06-2012
+# public domain
 #
 DB=i2p.mtn
-TMP=/usr/tmp/i2platest$$
+TMP=/var/tmp/i2platest$$
+TMP2=/var/tmp/i2platestx$$
 
 mtn list branches --no-standard-rcfiles -d $DB > $TMP
 for i in `cat $TMP`
 do
-	#echo $i
-	mtn head --no-standard-rcfiles -d $DB -b $i
-	echo
+	echo -n "${i}: " >> $TMP2
+	mtn head --no-standard-rcfiles -d $DB -b $i 2> /dev/null | cut -d ' ' -f 2- >> $TMP2
 done
 
-rm -f $TMP
+# adjust if you aren't in a US locale :)
+#        year           month          day            am/pm hour          minute
+sort -b -k 3.7nb,3.10nb -k 3.1nb,3.2nb -k 3.4nb,3.5nb -k 5 -k 4.1nb,4.2nb -k 4.4nb,4.5nb $TMP2
+
+rm -f $TMP $TMP2
