e64784316058f3ecb794c077b38b6eacf55836a0
zzz <zzz@mail.i2p>
2020-05-21 13:26:36 +0000

    config advanced on/off

---
+++ testing/advon.sh
@@ -0,0 +1,9 @@
+#
+# Turn on routerconsole.advanced
+# Run before starting router.
+# zzz 12/2011
+#
+grep -v routerconsole.advanced ~/.i2p/router.config > /tmp/foo$$
+echo 'routerconsole.advanced=true' >> /tmp/foo$$
+mv /tmp/foo$$ ~/.i2p/router.config
+echo 'Advanced console enabled'
