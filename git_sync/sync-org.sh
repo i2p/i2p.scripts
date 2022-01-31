4e778925b72cd7763245c6caa9b4823a8b69ae1c
idk <hankhill19580@gmail.com>
2022-01-31 12:21:28 -0500

    Don't loop in the sync-org script so it doesn't stall on a merge conflict

--- git_sync/sync-org.sh
+++ git_sync/sync-org.sh
@@ -13,31 +13,31 @@ for URL in $URLLIST; do
   if [ -d "$CLONEDIR" ]; then
     echo "Changing to $CLONEDIR"
     cd "$CLONEDIR" || exit 1
-    while true; do
+    #while true; do
       # pull all the updates
       echo "Pulling updates for $CLONEDIR..."
       git pull --all && break
-    done
-    while true; do
+    #done
+    #while true; do
       # pull all the tags
       echo "Pulling tags for $CLONEDIR..."
       git pull origin --tags && break
-    done
-    while true; do
+    #done
+    #while true; do
       # push all the updates
       echo "Merging updates for $CLONEDIR..."
       git pull origin master && break
-    done
-    while true; do
+    #done
+    #while true; do
       # push all the branches
       echo "Pushing updates for $CLONEDIR..."
       git push github --all && break
-    done
-    while true; do
+    #done
+    #while true; do
       # push all the tags
       echo "Pushing tags for $CLONEDIR..."
       git push github --tags && break
-    done
+    #done
     echo "Returning home"
     cd "$HERE" || exit 1
   fi
