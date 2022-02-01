193095fbfc3b27b33bf7693a53d7513bf0da26b8
idk <hankhill19580@gmail.com>
2022-02-01 03:16:16 -0500

    Try ten times to do each step, then move on.

--- git_sync/sync-org.sh
+++ git_sync/sync-org.sh
@@ -6,6 +6,8 @@ URLLIST=$(./list-org-cloneurls.sh)
 
 HERE=$(CDPATH= cd -- "$(dirname -- "$0")" && pwd)
 
+DO="1 2 3 4 5 6 7 8 9 10"
+
 for URL in $URLLIST; do
   BASEURL=$(echo $URL | sed "s|github.com:$ORG|i2pgit.org:i2p-hackers|g" | sed 's|i2p\.plugins\.i2psnark-rpc|i2psnark-rpc|g')
   CLONEDIR=$(echo $URL | sed "s|git@github.com:$ORG/||g" | sed 's|.git||g')
@@ -13,31 +15,31 @@ for URL in $URLLIST; do
   if [ -d "$CLONEDIR" ]; then
     echo "Changing to $CLONEDIR"
     cd "$CLONEDIR" || exit 1
-    #while true; do
+    for d in $DO; do
       # pull all the updates
-      echo "Pulling updates for $CLONEDIR..."
+      echo "Pulling updates for $CLONEDIR... attempt $d"
       git pull --all && break
-    #done
-    #while true; do
+    done
+    for d in $DO; do
       # pull all the tags
-      echo "Pulling tags for $CLONEDIR..."
+      echo "Pulling tags for $CLONEDIR... attempt $d"
       git pull origin --tags && break
-    #done
-    #while true; do
+    done
+    for d in $DO; do
       # push all the updates
-      echo "Merging updates for $CLONEDIR..."
+      echo "Merging updates for $CLONEDIR... attempt $d"
       git pull origin master && break
-    #done
-    #while true; do
+    done
+    for d in $DO; do
       # push all the branches
-      echo "Pushing updates for $CLONEDIR..."
+      echo "Pushing updates for $CLONEDIR... attempt $d"
       git push github --all && break
-    #done
-    #while true; do
+    done
+    for d in $DO; do
       # push all the tags
-      echo "Pushing tags for $CLONEDIR..."
+      echo "Pushing tags for $CLONEDIR... attempt $d"
       git push github --tags && break
-    #done
+    done
     echo "Returning home"
     cd "$HERE" || exit 1
   fi
